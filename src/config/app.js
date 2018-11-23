bc4c701f27a79b2114ba1b5ae15782ecbf275feb
Andrew Zamojc <azamojc@pagerduty.com>
2018-11-23 14:42:09 -0500

    tidy up

--- src/config/app.js
+++ src/config/app.js
@@ -1,4 +1,3 @@
-import developmentConfig from './development';
 import environmentVariables from '../scripts/environment-variables';
 
 const appConfig = {
@@ -6,10 +5,6 @@ const appConfig = {
   API_BASE_PATH: '/api/v1/',
   NON_API_BASE_PATH: '/',
   ENVIRONMENT: environmentVariables.NODE_ENV,
-  ...(this.ENVIRONMENT === 'development' ? developmentConfig : {})
 };
-
-console.log('===================================');
-console.log(appConfig);
 
 export default appConfig;
