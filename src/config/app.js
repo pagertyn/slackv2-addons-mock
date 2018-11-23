5592a7c45d8513e2b88ea20398a1fbd08f55a24c
Andrew Zamojc <azamojc@pagerduty.com>
2018-11-23 16:25:45 -0500

    fix lint issue

--- src/config/app.js
+++ src/config/app.js
@@ -5,7 +5,8 @@ const appConfig = {
   API_BASE_PATH: '/api/v1/',
   NON_API_BASE_PATH: '/',
 
-  // CRA says NODE_ENV is "development" for `react-scripts start` and "production" for `react-scripts build`
+  // "development" for `react-scripts start`
+  // "production" for `react-scripts build`
   ENVIRONMENT: environmentVariables.NODE_ENV,
 };
 
