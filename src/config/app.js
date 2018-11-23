3274543312558e1d2aa74f5fec489a108b34ef92
Andrew Zamojc <azamojc@pagerduty.com>
2018-11-23 14:53:20 -0500

    add a comment

--- src/config/app.js
+++ src/config/app.js
@@ -4,6 +4,8 @@ const appConfig = {
   FEATURE_FLAG: 'react_skeleton',
   API_BASE_PATH: '/api/v1/',
   NON_API_BASE_PATH: '/',
+
+  // CRA says NODE_ENV is "development" for `react-scripts start` and "production" for `react-scripts build`
   ENVIRONMENT: environmentVariables.NODE_ENV,
 };
 
