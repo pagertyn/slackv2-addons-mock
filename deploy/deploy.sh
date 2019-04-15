14b91cc87f043b079acf512b8705fa8488a23d3b
Chris Micacchi <cmicacchi@pagerduty.com>
2019-04-15 11:28:55 -0400

    Use better Cache-Control headers

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,7 +3,7 @@ npm run build
 
 echo 'Copying build to s3'
 cp public/index-dev.html build
-aws s3 cp build/ $DEPLOY_TO --recursive --acl public-read --cache-control 'max-age=0, no-cache'
+aws s3 cp build/ $DEPLOY_TO --recursive --acl public-read --cache-control 'public, max-age=31536000'
 
 echo 'Activating deployed version'
 echo $COMMIT_SHA >> sha.txt && aws s3 cp sha.txt $DEPLOY_VERSION_FILE_TO --acl public-read
